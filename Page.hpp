70b30ac3f1410e1e49b402c413d86d498d483abd
Or Yitshak <tur3103@gmail.com>
2022-03-22 21:56:19 +0200

    page file update

--- Page.hpp
+++ Page.hpp
@@ -1,15 +1,31 @@
 #pragma once
-#include<iostream>
-#include<string>
-#include<vector>
-#include "Line.hpp"
+#include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
 class Page
 {
-private:
-    vector<Line> lines;
-	
 public:
-	
+    // vector<Line> lines;
+
+    // vector<Line> vec();
+    int num_of_line;
+    int len_of_line;
+    Page()
+    {
+        num_of_line = 10;
+        len_of_line = 100;
+        // vector<vector<int>> vec( 10 , vector<int> (100));
+
+        
+        // for (size_t i = 0; i < num_of_line; i++)
+        // {
+        //     for (size_t j = 0; j < len_of_line; j++)
+        //     {
+        //         vec[i][j] = '_';
+        //     }            
+        // }        
+    }
+
 };
